00e731967ad1db93c1f09371e97647bffb0141dd
Kyle <tokisadamizuduki@gmail.com>
2017-12-14 16:58:16 +0800

    SWAPI ver2

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,7 +1,14 @@
 <template>
 <div id="app">
 	<h3>{{title}}</h3>
-	<Character></Character>
+	<div class="col-md-12">
+		<Character 
+		v-for="(id,index) in initial_ids"
+		:id="id"
+		:key="index"
+		>	
+		</Character>
+	</div>
 </div>
 </template>
 
@@ -11,7 +18,8 @@ export default {
 	name: 'app',
 	data(){
 		return {
-			title: 'Generate your team'
+			title: 'Generate your team',
+			initial_ids: ['2','5','8']
 		}
 	},
 	components: {
--- applications/starbase/src/components/Character.vue
+++ applications/starbase/src/components/Character.vue
@@ -1,26 +1,32 @@
 <template>
-	<div>{{character}}</div>
+	<div @click="switchCharacter">{{character}}</div>
 </template>
 
 <script>
 	export default {
 		name: 'app',
+		props: ['id'],
 		data(){
 			return {
 				character: {}
 			}
 		},
 		methods: {
-			fetchCharacter(){
-				fetch("https://swapi.co/api/people/1/",{
+			fetchCharacter(id){
+				fetch(`https://swapi.co/api/people/${id}/`,{
 					method:'GET'
 				})
 				.then(response => response.json())
 				.then(json=>this.character = json)
+			},
+			switchCharacter(){
+				let random_id = Math.floor(Math.random()*83)+1
+				this.fetchCharacter(random_id)
+				console.log(random_id)
 			}
 		},
 		created(){
-			this.fetchCharacter();
+			this.fetchCharacter(this.id)
 		}
 	}
 </script>
